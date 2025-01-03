91a19c17c5adc745a5840cd87f541ab93a8bc2d6
Ksgaksbsye <naincyprajapati122008@gmail.com>
2025-01-03 11:28:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "GOOGLE_MUSIC_REBOT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "GOOGLE_MUSIC_UPDATE")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "GOOGLE_MUSIC_SUPPORT")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
